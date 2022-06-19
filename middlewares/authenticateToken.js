09b1d198e4cb4129a9c0d00b203a2a8d2d1e9445
Husnil K <husnil.k@gmail.com>
2022-06-20 00:05:17 +0700

    Added midleware

---
+++ middlewares/authenticateToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+function authenticateToken(req, res, next) {
+
+    dotenv.config();
+    let secret = process.env.JWT_SECRET;
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    
+    if (token == null) return res.sendStatus(401)
+    
+    jwt.verify(token, secret, (err, user) => {
+        console.log(err)
+        
+        if (err) return res.sendStatus(403)
+        
+        req.user = user
+        
+        next()
+    })
+}
+
+module.exports = authenticateToken;
