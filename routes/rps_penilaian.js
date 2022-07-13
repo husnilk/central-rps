a5d6eab20b678abff884c46fa3abd1699ffa5ffa
husnilk <husnil.k@gmail.com>
2022-07-13 12:31:37 +0700

    Added: Komponen Penilaian

--- routes/rps_penilaian.js
+++ routes/rps_penilaian.js
@@ -4,65 +4,165 @@ var router = express.Router();
 
 var authenticateToken = require('../middlewares/authenticateToken');
 const CoursePlanAssessment = require('../models/course_plan_assessments');
+const Lecturer = require('../models/lecturers');
 var sequelize = require('../utils/connect')
 
 router.get('/:rpsId/assessments/', authenticateToken, async function (req, res) {
-    var rpsId = req.params.rpsId;
-        var percentage = req.body.percentage;
-        var name = req.body.name;
-        var datetime = new Date().toISOString();
+    var user = req.user;
+    let rpsId = req.params.rpsId;
+    
+    try {
+        var lecturer = await Lecturer.findByPk(user.id);
+        console.log(lecturer.id)
+        var assessments = await sequelize.query('SELECT '+
+        'course_plan_assessments.id,' + 
+        'course_plan_assessments.name,' +
+        'course_plan_assessments.percentage ' +
+        'FROM course_plan_assessments '+
+        'WHERE course_plan_id = :course_plan_id',
+        {
+            replacements: { course_plan_id: rpsId},
+            type: QueryTypes.SELECT
+        });
         
-        if(rpsId == null || name == null || percentage == null ){
-            return res.json({
-                status: "400",
-                message: "Field code, name dan CPL tidak boleh kosong",
-                id: null,
-                datetime: datetime
-            });
-        }
-        try{
-            let assessments = await CoursePlanAssessment.create({
-                course_plan_id : rpsId,
-                name: name,
-                percentage: percentage
-            });
-            
-            res.json({
-                status: "200",
-                message: "Data Komponen Penilaian berhasil ditambahkan",
-                id: cpmk.id,
-                datetime: datetime
-            });
-        }catch(error){
-            console.log(error);
-            res.json(error);
-        }
+        console.log(assessments);
+    }catch(error){
+        console.log("Ok");
+    }
+    if(assessments == undefined){
+        counts = 0;
+        assessments = {};
+    }else{
+        counts = assessments.length;
+    }
+    var datetime = new Date().toISOString();
+    var response = {
+        counts: counts,
+        datetime: datetime,
+        assessments : assessments
+    };
+    
+    res.json(response);
     
 });
 
 /**
- * Tambah Komponen Penilaian
- */
+* Tambah Komponen Penilaian
+*/
 router.post('/:rpsId/assessments', authenticateToken, async (req, res) => {
     let rpsId = req.params.rpsId;
-    let course_name = req.params.name;
-    let course_percentage = req.params.percentage;
+    let course_plan_id = req.body.course_plan_id;
+    let name = req.body.name;
+    let percentage = req.body.percentage;
+    
+    console.log(name, percentage);
 
+    if(course_plan_id == null || name == null || percentage == null){
+        return res.json({
+            status: "400",
+            message: "Nama dan Persentase harus diisi",
+            id: null,
+            datetime: new Date().toISOString()
+        })
+    }
+    
     try{
-        var assessments = await CoursePlanAssessment.create({
-
+        var assessment = await CoursePlanAssessment.create({
+            course_plan_id: course_plan_id,
+            name: name,
+            percentage: percentage,
+            flag: 1
         });
+        
+        return res.json({
+            status: "200",
+            message: "Data Komponen Penilaian berhasil ditambahkan",
+            id: assessment.id,
+            datetime: new Date().toISOString()
+        });
+    }catch(error){
+        console.log(error);
+        return res.json(error);
+    }
+});
+
+/**
+ * Update Komponen Penilaian
+ */
+router.put('/:rpsId/assessments/:assessmentId', authenticateToken, async (req, res) => {
+    let assessmentId = req.body.assessmentId;
+    let rpsId = req.body.rpsId;
+    let name = req.body.name;
+    let percentage = req.body.percentage;
+    console.log(req.params);
+    
+    console.log(name, percentage);
+
+    if(name == null || percentage == null){
+        return res.json({
+            status: "400",
+            message: "Nama dan Persentase harus diisi",
+            id: null,
+            datetime: new Date().toISOString()
+        })
+    }
+    
+    try{
+        var assessment = await CoursePlanAssessment.findByPk(assessmentId);
+        assessment.name = name;
+        assessment.percentage = percentage;
+        await assessment.save();
 
         return res.json({
             status: "200",
-            message: "Data CPMK berhasil ditambahkan",
-            id: cpmk.id,
-            datetime: datetime
+            message: "Data Komponen Penilaian berhasil diupdate",
+            id: assessment.id,
+            datetime: new Date().toISOString()
         });
     }catch(error){
-
+        console.log(error);
+        return res.json(error);
     }
-    
+});
+
+/**
+ * Destroy Komponen Penilaian
+ */
+router.delete('/:rpsId/assessments/:assessmentId', authenticateToken, async (req, res) => {
+    let assessmentId = req.params.assessmentId;
+    let rpsId = req.params.rpsId;
+
+    console.log(assessmentId);
+
+    const t = await sequelize.transaction();
+    try{
+        await sequelize.query('DELETE FROM course_plan_detail_assessments WHERE course_plan_assessment_id = :assessment_id', 
+            {
+                replacements: {
+                    assessment_id: assessmentId
+                },
+                type: QueryTypes.DELETE
+            });
+        await sequelize.query('DELETE FROM course_plan_assessments WHERE id = :assessment_id ', 
+        {
+            replacements: {
+                assessment_id: assessmentId
+            },
+            type: QueryTypes.DELETE
+        });
+        t.commit();
+        return res.json({
+            status: "200",
+            message: "Komponen penilaian berhasil dihapus",
+            id: assessmentId,
+            datetime: new Date().toISOString()
+        })
+    }catch(error){
+        console.log(error);
+        await t.rollback();
+        return res.json(error);
+    }
+
 });
 
 module.exports = router;
